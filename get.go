319536a62ef9c4214d0246b56b208965713ab00d
Quvonchbek Murodov <quvonchbe05@gmail.com>
2024-12-05 10:52:47 +0500

    added get request

---
+++ get.go
@@ -0,0 +1,34 @@
+package requests
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func Get[T any](url string, obj *T) (*T, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			fmt.Printf("Error closing response body: %s\n", err)
+		}
+	}(resp.Body)
+
+	fmt.Println(resp)
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := json.Unmarshal(body, obj); err != nil {
+		return nil, err
+	}
+
+	return obj, nil
+}
